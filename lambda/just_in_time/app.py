9750d0c7572e586c7efdbb796b2d137d14f35e3c
elsholz <hendrik.lankers.hl@googlemail.com>
2023-01-30 21:40:05 +0100

    add authorization

--- lambda/just_in_time/app.py
+++ lambda/just_in_time/app.py
@@ -1,6 +1,60 @@
 import json
 
-# import requests
+
+import json
+from six.moves.urllib.request import urlopen
+from functools import wraps
+
+from jose import jwt
+
+
+AUTH0_DOMAIN = 'dev-twa5tnu1.eu.auth0.com'
+API_AUDIENCE = 'https://just-in-ti.me/api'
+ALGORITHMS = ["RS256"]
+
+
+def check_auth(evt):
+    token = evt.get('Authorization', ' ').split(' ')[1]
+    if not token:
+        return
+
+    jsonurl = urlopen(
+        "https://dev-twa5tnu1.eu.auth0.com/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer="https://dev-twa5tnu1.eu.auth0.com/"
+            )
+        except jwt.ExpiredSignatureError:
+            print({"code": "token_expired",
+                   "description": "token is expired"}, 401)
+        except jwt.JWTClaimsError:
+            print({"code": "invalid_claims",
+                   "description":
+                   "incorrect claims,"
+                   "please check the audience and issuer"}, 401)
+        except Exception:
+            print({"code": "invalid_header",
+                   "description":
+                   "Unable to parse authentication"
+                   " token."}, 401)
+        print(payload)
 
 
 def lambda_handler(event, context):
@@ -25,18 +79,11 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    check_auth(event)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }

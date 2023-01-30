d8d219e0879da0a5f9dc0cf8a874e4d258a58310
elsholz <hendrik.lankers.hl@googlemail.com>
2023-01-30 23:11:25 +0100

    fix code

--- lambda/just_in_time/auth.py
+++ lambda/just_in_time/auth.py
@@ -1,19 +1,6 @@
 from json import dumps
 from auth0.authentication.token_verifier import TokenVerifier, AsymmetricSignatureVerifier
 
-domain = 'myaccount.auth0.com'
-client_id = 'exampleid'
-
-# After authenticating
-id_token = auth_result['id_token']
-
-jwks_url = 'https://{}/.well-known/jwks.json'.format(domain)
-issuer = 'https://{}/'.format(domain)
-
-sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
-tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience=client_id)
-tv.verify(id_token)
-
 
 def check_auth(evt):
     print('Entered check_auth function')
@@ -31,58 +18,21 @@ def check_auth(evt):
     else:
         print('No Auth Header provided')
 
-    return 'test123'
+    try:
+        AUTH0_DOMAIN = 'dev-twa5tnu1.eu.auth0.com'
+        API_AUDIENCE = 'https://just-in-ti.me/api'
 
-# from six.moves.urllib.request import urlopen
-# from functools import wraps
-#
-# from jose import jwt
-#
-#
-# AUTH0_DOMAIN = 'dev-twa5tnu1.eu.auth0.com'
-# API_AUDIENCE = 'https://just-in-ti.me/api'
-# ALGORITHMS = ["RS256"]
-#
-#
-# def check_auth(evt):
-#     token = evt.get('Authorization', ' ').split(' ')[1]
-#     if not token:
-#         return
-#
-#     jsonurl = urlopen(
-#         "https://dev-twa5tnu1.eu.auth0.com/.well-known/jwks.json")
-#     jwks = json.loads(jsonurl.read())
-#     unverified_header = jwt.get_unverified_header(token)
-#     rsa_key = {}
-#     for key in jwks["keys"]:
-#         if key["kid"] == unverified_header["kid"]:
-#             rsa_key = {
-#                 "kty": key["kty"],
-#                 "kid": key["kid"],
-#                 "use": key["use"],
-#                 "n": key["n"],
-#                 "e": key["e"]
-#             }
-#     if rsa_key:
-#         try:
-#             payload = jwt.decode(
-#                 token,
-#                 rsa_key,
-#                 algorithms=ALGORITHMS,
-#                 audience=API_AUDIENCE,
-#                 issuer="https://dev-twa5tnu1.eu.auth0.com/"
-#             )
-#         except jwt.ExpiredSignatureError:
-#             print({"code": "token_expired",
-#                    "description": "token is expired"}, 401)
-#         except jwt.JWTClaimsError:
-#             print({"code": "invalid_claims",
-#                    "description":
-#                    "incorrect claims,"
-#                    "please check the audience and issuer"}, 401)
-#         except Exception:
-#             print({"code": "invalid_header",
-#                    "description":
-#                    "Unable to parse authentication"
-#                    " token."}, 401)
-#         print(payload)
+        jwks_url = f'https://{AUTH0_DOMAIN}/.well-known/jwks.json'
+        issuer = f'https://{AUTH0_DOMAIN}/'
+
+        token = auth_header.split(' ')[-1]
+
+        sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
+        tv = TokenVerifier(
+            signature_verifier=sv, issuer=issuer, audience=API_AUDIENCE
+        )
+        print('Token verify:', tv.verify(token))
+    except Exception as e:
+        print("Error occured:", e)
+
+    return 'test123'

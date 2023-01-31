88b1cb962eb89638fc64324a4f6c1b42c3123803
elsholz <hendrik.lankers.hl@googlemail.com>
2023-01-31 23:47:56 +0100

    text key store

--- lambda/just_in_time/just_in_time.py
+++ lambda/just_in_time/just_in_time.py
@@ -19,6 +19,8 @@ get_secret_value_response = client.get_secret_value(
     SecretId=secret_name
 )['SecretString']
 
+print('Secret String:', get_secret_value_response)
+
 DB_USER = get_secret_value_response['DB_USER']
 DB_PASSWORD = get_secret_value_response['DB_PASSWORD']
 

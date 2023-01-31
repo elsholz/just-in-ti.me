e00bccde41a016edb49f02b6e0b7f977c90f6f61
elsholz <hendrik.lankers.hl@googlemail.com>
2023-01-31 23:14:27 +0100

    add mongodb

---
+++ lambda/just_in_time/just_in_time.py
@@ -0,0 +1,71 @@
+import json
+import auth
+from pymongo import MongoClient
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+secret_name = "JITKeys"
+region_name = "eu-central-1"
+
+session = boto3.session.Session()
+client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name,
+)
+
+get_secret_value_response = client.get_secret_value(
+    SecretId=secret_name
+)
+
+DB_USER = get_secret_value_response['DB_USER']
+DB_PASSWORD = get_secret_value_response['DB_PASSWORD']
+# if 'SecretString' in get_secret_value_response:
+#     text_secret_data = get_secret_value_response['SecretString']
+# else:
+#     binary_secret_data = get_secret_value_response['SecretBinary']
+
+
+client = MongoClient(
+    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.mtoqrjz.mongodb.net/just-in-time"
+)
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    print('Check Auth returned:', auth.check_auth(event))
+    userid = auth.check_auth(event)
+
+    db = client['just-in-time']
+    print("Getting Items from DB")
+    for user in db.userdata.find():
+        print(user)
+    print("End Items from DB")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Ja moin",
+        }),
+    }
